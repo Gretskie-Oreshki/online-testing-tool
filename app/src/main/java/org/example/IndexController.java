7b2d22851e2683adcbbeb1d8fae0762b571bf3e9
Imil <ibragimovimil0@gmail.com>
2025-03-15 17:06:53 +0300

    [refactor] Update Index to show simple html

--- app/src/main/java/org/example/IndexController.java
+++ app/src/main/java/org/example/IndexController.java
@@ -1,12 +1,12 @@
 package org.example;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class IndexController {
   @GetMapping("/")
   public String index() {
-    return "Hello, World!";
+    return "index";
   }
 }

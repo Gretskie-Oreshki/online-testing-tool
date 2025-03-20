ece77020284fb6aa5b8d6e42dfccbf8f8141c642
Strelyaev R. <roman.strelyaeff@gmail.com>
2025-03-20 17:34:10 +0300

    [refactor] Controller example

--- app/src/main/java/org/example/TestController.java
+++ app/src/main/java/org/example/TestController.java
@@ -1,28 +1,71 @@
 package org.example;
 
-
-import org.example.model.Test;
+import org.example.model.Guest;
+import org.example.model.Result;
+import org.example.model.TestEntity;
+import org.example.repository.GuestRepository;
+import org.example.repository.ResultRepository;
 import org.example.repository.TestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ *                       ЭТОТ КЛАСС ЯВЛЯЕТСЯ ПРИМЕРОМ И ПОДЛЕЖИТ УДАЛЕНИЮ.
+ *                                 КТО ИСПОЛЬЗУЕТ ТОТ ГЕЙ
+ */
+
 @Controller
-@RequestMapping(path="/tests") // Нужно будет поменять мужики не забудьте ;)
+@RequestMapping(path="/api")
 public class TestController {
 
     @Autowired
     private TestRepository testRepository;
 
-    @GetMapping(path="/get")
-    public @ResponseBody Iterable<Test> getAllTests() {
+    @Autowired
+    private GuestRepository guestRepository;
+
+    @Autowired
+    private ResultRepository resultRepository;
+
+    @GetMapping("/results")
+    public @ResponseBody Iterable<Result> getAllResults() {
+        return resultRepository.findAll();
+    }
+
+    @GetMapping("/guests")
+    public @ResponseBody Iterable<Guest> getAllGuests() {
+        return guestRepository.findAll();
+    }
+
+    @GetMapping("/tests")
+    public @ResponseBody Iterable<TestEntity> getAllTests() {
         return testRepository.findAll();
     }
 
-    @PostMapping(path="/post")
+    @PostMapping(path="/test")
     public @ResponseBody String createTest(@RequestParam String name) {
-        Test test = new Test(name, "");
+        TestEntity test = new TestEntity(name, "");
         testRepository.save(test);
         return "Saved";
+    }
+
+    @PostMapping("/guest")
+    public @ResponseBody String createGuest() {
+        Guest guest = new Guest();
+        guestRepository.save(guest);
+        return "Saved"; // Перенаправление на страницу с гостями
+    }
+
+    // Метод для создания результата
+    @PostMapping("/result")
+    public @ResponseBody String createResult(@RequestParam Long guestId, @RequestParam Long testId, @RequestParam int resultValue) {
+        Guest guest = guestRepository.findById(guestId).orElseThrow(() -> new RuntimeException("Guest not found"));
+        TestEntity testEntity = testRepository.findById(testId).orElseThrow(() -> new RuntimeException("Test not found"));
+
+        Result result = new Result(guest, testEntity, resultValue);
+        resultRepository.save(result);
+
+        return "redirect:/api/results"; // Перенаправление на страницу с результатами
     }
 }

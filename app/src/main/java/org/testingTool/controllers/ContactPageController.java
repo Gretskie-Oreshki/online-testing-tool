5b14054ce0dae7008fe711ad07f3d1af455823e6
Imil Ibragimov <141638913+StantStantov@users.noreply.github.com>
2025-03-25 20:33:50 +0300

    Revert "Added two html templates for guest profile and log in. "

--- app/src/main/java/org/testingTool/controllers/ContactPageController.java
+++ app/src/main/java/org/testingTool/controllers/ContactPageController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
-  @GetMapping("/login")
-  public String login() {
-    return "login";
+public class ContactPageController {
+  @GetMapping("/contact")
+  public String contact() {
+    return "contact";
   }
 }

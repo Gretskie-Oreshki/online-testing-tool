d0a35eb6fc54cbe2a02fc10a3c67c50512db9fc3
osakasonyboy@gmail.com <sensh0god@gmail.com>
2025-03-29 21:10:14 +0300

    fetch upstream

--- app/src/main/java/org/testingTool/controllers/AdminController.java
+++ app/src/main/java/org/testingTool/controllers/AdminController.java
@@ -1,5 +1,7 @@
 package org.testingTool.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.testingTool.model.AdminEntity;
 import org.testingTool.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/admin")
+@PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
 public class AdminController {
   @Autowired
   private AdminRepository adminRepository;
 
-  @GetMapping("/admin")
+  @GetMapping("/")
   public String admin(@RequestParam(value = "id") Long id, Model model) {
     AdminEntity admin = adminRepository.findById(id).orElseThrow(() -> new RuntimeException("Admin does not exist"));
+    model.addAttribute("admin", admin.getID());
     return "admin";
   }
+
 }

e293b6ad144f082254e25a1875ba0bf772df430e
deadshumz <shumzz@bk.ru>
2025-04-24 00:43:05 +0300

    feat: refactor services

---
+++ app/src/main/java/org/testingTool/services/UserService.java
@@ -0,0 +1,36 @@
+package org.testingTool.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.testingTool.model.Role;
+import org.testingTool.model.UserEntity;
+import org.testingTool.repository.UserRepository;
+
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  private String generateUid() {
+    Random random = new Random();
+    return String.format("%04d-%04d", random.nextInt(10000), random.nextInt(10000));
+  }
+
+  public void addUser(UserEntity user) {
+    String uid = generateUid();
+
+    while (userRepository.existsByUid(uid)) {
+      uid = generateUid();
+    }
+
+    user.setUid(uid);
+    user.setRole(Role.USER);
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    userRepository.save(user);
+  }
+}

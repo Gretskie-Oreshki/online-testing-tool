1abca73c132d6d59a0e2e488ccf90f1a4a763422
osakasonyboy@gmail.com <sensh0god@gmail.com>
2025-04-24 18:05:09 +0300

    added TestCreationController.java and create_test.html

---
+++ app/src/main/java/org/testingTool/controllers/TestCreationController.java
@@ -0,0 +1,42 @@
+package org.testingTool.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.testingTool.model.TestEntity;
+import org.testingTool.repository.TestRepository;
+import org.testingTool.dataTransferObjects.TestDataTransferObject;
+
+@Controller
+@RequestMapping("/test/constructor")
+//@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+public class TestCreationController {
+  private final TestRepository testRepository;
+
+  public TestCreationController(TestRepository testRepository) {
+    this.testRepository = testRepository;
+  }
+
+  @GetMapping
+  public String showConstructor(Model model) {
+    model.addAttribute("test", new TestDataTransferObject());
+    return "create_test";
+  }
+
+  @PostMapping
+  public String saveTest(@ModelAttribute("test") TestDataTransferObject testDTO) {
+    TestEntity test = new TestEntity();
+    test.setName(testDTO.getTestName());
+    test.setQuestions(testDTO.getListOfQuestions());
+    testRepository.save(test);
+    return "redirect:/test/constructor/success";
+  }
+
+  @GetMapping("/success")
+  public String showSuccessPage() {
+    return "success";
+  }
+}

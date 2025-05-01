b2d29111e0a1c7962cf7b62f9069042ded8663a4
osakasonyboy@gmail.com <sensh0god@gmail.com>
2025-05-01 23:37:22 +0300

    imports

--- app/src/main/java/org/testingTool/model/TestEntity.java
+++ app/src/main/java/org/testingTool/model/TestEntity.java
@@ -1,6 +1,14 @@
 package org.testingTool.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;

16e635abf810570545406f935185a17c3f32ebcc
osakasonyboy@gmail.com <sensh0god@gmail.com>
2025-05-01 22:52:27 +0300

    added material entity + material repo + edited test entity

---
+++ app/src/main/java/org/testingTool/model/MaterialEntity.java
@@ -0,0 +1,29 @@
+package org.testingTool.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "materials")
+@NoArgsConstructor
+@Setter
+@Getter
+public class MaterialEntity {
+
+  @Id
+  @GeneratedValue
+  @Setter(AccessLevel.NONE)
+  private Long id;
+
+  private String fileName; // человеческое имя файла cg.pdf
+  private String filePath; // путь к файлу с уже измененным названием через uuid asdasdasdadsasda.pdf
+
+  @ManyToMany(mappedBy = "materials")
+  private List<TestEntity> tests = new ArrayList<>();
+}

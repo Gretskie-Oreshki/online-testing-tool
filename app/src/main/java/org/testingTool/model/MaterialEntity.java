2f11ad39548b33391a44988723550cabc44d4eea
osakasonyboy@gmail.com <sensh0god@gmail.com>
2025-05-01 23:37:22 +0300

    imports

--- app/src/main/java/org/testingTool/model/MaterialEntity.java
+++ app/src/main/java/org/testingTool/model/MaterialEntity.java
@@ -1,6 +1,10 @@
 package org.testingTool.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;

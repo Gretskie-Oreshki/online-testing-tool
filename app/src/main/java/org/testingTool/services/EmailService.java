0546afb697f0250e564d6b3aa2a719a14950af9f
Imil <ibragimovimil0@gmail.com>
2025-05-03 14:28:58 +0300

    [feat] Add textTemplate to EmailService

--- app/src/main/java/org/testingTool/services/EmailService.java
+++ app/src/main/java/org/testingTool/services/EmailService.java
@@ -1,10 +1,12 @@
 package org.testingTool.services;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.testingTool.model.UserAnswerEntity;
 
 @Service
 public class EmailService {
@@ -13,12 +15,25 @@ public class EmailService {
   @Value("${EMAIL_ADDRESS}")
   private String senderEmailAddress;
 
-  public void sendResultsMail(String to, String subject) {
+  private String emailSubjectTemplate = "\"%s\" прошел \"%s\"";
+  private String emailTextTemplate = "Пользователь \"%s\" прошел тест \"%s\"\n\nРезультат: %d/%d";
+
+  public void sendResultsMail(
+      String toEmail, String userUid, String testName, List<UserAnswerEntity> userAnswers) {
     SimpleMailMessage msg = new SimpleMailMessage();
     msg.setFrom(senderEmailAddress);
-    msg.setTo(to);
-    msg.setSubject(subject);
-    msg.setText("");
+    msg.setTo(toEmail);
+    msg.setSubject(String.format(emailSubjectTemplate, userUid, testName));
+
+    long answersAmount = userAnswers.size();
+    long rightAnswersAmount =
+        userAnswers.stream().filter(userAnswer -> userAnswer.getAnswer().isRight()).count();
+
+    int maxPercent = 100;
+    int rightAnswersPercent = Math.round(((float) rightAnswersAmount / answersAmount) * maxPercent);
+
+    msg.setText(
+        String.format(emailTextTemplate, userUid, testName, rightAnswersPercent, maxPercent));
 
     mailSender.send(msg);
   }

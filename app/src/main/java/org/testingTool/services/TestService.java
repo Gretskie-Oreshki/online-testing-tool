9c1c416e71d7b8222a1ef5f8b302907a999c096a
deadshumz <shumzz@bk.ru>
2025-05-04 16:24:41 +0300

    feat: add test service

---
+++ app/src/main/java/org/testingTool/services/TestService.java
@@ -0,0 +1,21 @@
+package org.testingTool.services;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.testingTool.model.TestEntity;
+import org.testingTool.repository.TestRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+
+  private final TestRepository testRepository;
+
+  public TestEntity getTestById(Long id) {
+    return testRepository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException("Test with id " + id + " not found"));
+  }
+}

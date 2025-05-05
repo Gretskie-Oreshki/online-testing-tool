8cf1c2ab646e5c829c30597e59195d27fa5f17b2
deadshumz <shumzz@bk.ru>
2025-05-05 22:30:53 +0300

    chore: add getters/setter to access entity

--- app/src/main/java/org/testingTool/model/UserTestAccessEntity.java
+++ app/src/main/java/org/testingTool/model/UserTestAccessEntity.java
@@ -1,15 +1,21 @@
 package org.testingTool.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.annotation.Nullable;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "user_test_access")
 public class UserTestAccessEntity {
   @Id
   @GeneratedValue
+  @Setter(AccessLevel.NONE)
   private Long id;
 
   @ManyToOne
@@ -18,7 +24,7 @@ public class UserTestAccessEntity {
   @ManyToOne
   private TestEntity test;
 
-  private boolean isPassed;
+  private Boolean isPassed;
 
   @Nullable
   private LocalDateTime datePassed;
--- app/src/main/java/org/testingTool/repository/UserTestAccessRepository.java
+++ app/src/main/java/org/testingTool/repository/UserTestAccessRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.testingTool.model.UserTestAccessEntity;
 
+import java.util.Optional;
+
 public interface UserTestAccessRepository extends CrudRepository<UserTestAccessEntity, Long> {
   @Query("SELECT COUNT(uta) > 0 FROM UserTestAccessEntity uta " +
       "WHERE uta.user.id = :userId " +
       "AND uta.test.id = :testId " +
       "AND uta.isPassed = false")
   boolean hasAccess(@Param("userId") Long userId, @Param("testId") Long testId);
+
+  Optional<UserTestAccessEntity> findByUser_IdAndTest_Id(Long userId, Long testId);
 }

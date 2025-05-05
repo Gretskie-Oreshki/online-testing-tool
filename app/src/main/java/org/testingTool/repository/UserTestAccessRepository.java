95ae246bef8059fb5f68bbf664669c9eaa246c8d
deadshumz <shumzz@bk.ru>
2025-05-05 19:49:09 +0300

    chore: refactor hasAccess method with query

--- app/src/main/java/org/testingTool/repository/UserTestAccessRepository.java
+++ app/src/main/java/org/testingTool/repository/UserTestAccessRepository.java
@@ -1,8 +1,14 @@
 package org.testingTool.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.testingTool.model.UserTestAccessEntity;
 
 public interface UserTestAccessRepository extends CrudRepository<UserTestAccessEntity, Long> {
-  boolean existsByUser_UidAndTest_IdAndIsPassedFalse(String uid, Long testId);
+  @Query("SELECT COUNT(uta) > 0 FROM UserTestAccessEntity uta " +
+      "WHERE uta.user.id = :userId " +
+      "AND uta.test.id = :testId " +
+      "AND uta.isPassed = false")
+  boolean hasAccess(@Param("userId") Long userId, @Param("testId") Long testId);
 }

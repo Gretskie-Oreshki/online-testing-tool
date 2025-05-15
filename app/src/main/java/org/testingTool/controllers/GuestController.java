57a56916016e39f4a46b6b04a29aeee278e86473
deadshumz <shumzz@bk.ru>
2025-05-16 01:15:58 +0300

    feat: add guest page

--- app/src/main/java/org/testingTool/controllers/GuestController.java
+++ app/src/main/java/org/testingTool/controllers/GuestController.java
@@ -3,20 +3,34 @@ package org.testingTool.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.testingTool.model.UserEntity;
+import org.testingTool.model.UserTestAccessEntity;
 import org.testingTool.repository.UserRepository;
+import org.testingTool.repository.UserTestAccessRepository;
+
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/guest")
-@PreAuthorize("hasAuthority('ROLE_USER')")
 @RequiredArgsConstructor
 public class GuestController {
 
   private final UserRepository userRepository;
+  private final UserTestAccessRepository accessRepository;
 
   @GetMapping("/")
-  public String guests() {
+  public String guests(Model model, Principal principal) {
+    UserEntity user = userRepository.findByUid(principal.getName()).orElseThrow();
+    List<UserTestAccessEntity> accesses = accessRepository.findAllByUser(user);
+
+    System.out.println(user.getUid());
+    System.out.println(accesses);
+
+    model.addAttribute("accesses", accesses);
     return "guest_index";
   }
 }

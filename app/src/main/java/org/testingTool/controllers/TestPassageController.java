29a848a8509a60906c97d96e968edf7c0b59096a
R.S. <roman.strelyaeff@gmail.com>
2025-05-18 18:16:13 +0300

    [feat] email service added to submitPassage

--- app/src/main/java/org/testingTool/controllers/TestPassageController.java
+++ app/src/main/java/org/testingTool/controllers/TestPassageController.java
@@ -13,11 +13,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.testingTool.dto.UserAnswerFormDto;
+import org.testingTool.model.Role;
 import org.testingTool.model.TestEntity;
+import org.testingTool.model.UserAnswerEntity;
 import org.testingTool.model.UserEntity;
 import org.testingTool.repository.UserRepository;
+import org.testingTool.services.EmailService;
 import org.testingTool.services.TestService;
 import org.testingTool.services.UserAnswerService;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,6 +32,7 @@ public class TestPassageController {
   private final TestService testService;
   private final UserAnswerService userAnswerService;
   private final UserRepository userRepository;
+  private final EmailService emailService;
 
   @PreAuthorize("@accessChecker.canPassTest(principal.username, #id)")
   @GetMapping("/{id}")
@@ -46,7 +52,17 @@ public class TestPassageController {
         userRepository
             .findByUid(userDetails.getUsername())
             .orElseThrow(() -> new EntityNotFoundException("Пользователь не найден"));
-    userAnswerService.saveAnswers(formDto, user.getId());
+    List<UserAnswerEntity> userAnswers = userAnswerService.saveAnswers(formDto, user.getId());
+
+    UserEntity admin = userRepository
+            .findByRole(Role.ADMIN)
+            .stream()
+            .findFirst()
+            .orElseThrow(() -> new EntityNotFoundException("Администратор не найден")); // Будем считать, что админ у нас пока один
+
+    //  String toEmail, String userUid, String testName, List<UserAnswerEntity> userAnswers
+    String testName = testService.getTestById(id).getName();
+    emailService.sendUserResultsToMail(admin.getEmail(), user.getUid(), testName, userAnswers);
 
     return "redirect:/tests/success";
   }
